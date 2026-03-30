9255f54a5ebde37d82b85c582f2f1f521ec4fecc
Twan <twanvanderloo@gmail.com>
2026-03-30 12:11:07 +0200

    fix: reset zoomScale on dismiss and simplify dragOffset to CGFloat

--- app/Sources/Views/QuestDetail/ImageViewerOverlay.swift
+++ app/Sources/Views/QuestDetail/ImageViewerOverlay.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct ImageViewerOverlay: View {
     @Binding var image: UIImage?
 
-    @State private var dragOffset: CGSize = .zero
+    @State private var dragOffset: CGFloat = 0
     @State private var zoomScale: CGFloat = 1.0
 
     private let dismissThreshold: CGFloat = 120
@@ -18,7 +18,7 @@ struct ImageViewerOverlay: View {
             if let img = image {
                 ZoomableScrollView(image: img, zoomScale: $zoomScale)
                     .ignoresSafeArea()
-                    .offset(y: max(dragOffset.height, 0))
+                    .offset(y: max(dragOffset, 0))
                     .simultaneousGesture(swipeDownGesture)
                     .onTapGesture {
                         guard zoomScale <= 1.0 else { return }
@@ -41,25 +41,25 @@ struct ImageViewerOverlay: View {
     }
 
     private var backgroundOpacity: Double {
-        max(0, 1.0 - Double(dragOffset.height) / 300.0)
+        max(0, 1.0 - Double(dragOffset) / 300.0)
     }
 
     private var swipeDownGesture: some Gesture {
         DragGesture(minimumDistance: 20)
             .onChanged { value in
                 guard zoomScale <= 1.0, value.translation.height > 0 else { return }
-                dragOffset = value.translation
+                dragOffset = value.translation.height
             }
             .onEnded { value in
                 guard zoomScale <= 1.0 else {
-                    withAnimation(.spring(response: 0.3)) { dragOffset = .zero }
+                    withAnimation(.spring(response: 0.3)) { dragOffset = 0 }
                     return
                 }
                 if value.translation.height > dismissThreshold ||
                    value.predictedEndTranslation.height > dismissThreshold {
                     dismiss()
                 } else {
-                    withAnimation(.spring(response: 0.3)) { dragOffset = .zero }
+                    withAnimation(.spring(response: 0.3)) { dragOffset = 0 }
                 }
             }
     }
@@ -67,7 +67,8 @@ struct ImageViewerOverlay: View {
     private func dismiss() {
         withAnimation(.easeInOut(duration: 0.2)) {
             image = nil
-            dragOffset = .zero
+            dragOffset = 0
+            zoomScale = 1.0
         }
     }
 }
